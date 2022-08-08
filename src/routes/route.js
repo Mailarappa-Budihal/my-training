86940e0e1cf804787915b9dce0dc618c7581d9e3
MailarappaBudihal <104186059+Mailarappa-Budihal@users.noreply.github.com>
2022-08-08 23:34:53 +0530

    Assignment get -api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,84 @@
-const express = require('express');
-const abc = require('../introduction/intro')
+const express = require('express')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
 
-    res.send('My second ever api!')
-});
+// get api Assignment
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
-})
-
+// 1. St question
+router.get('/get-movies',
+    function(req, res) {
+        let movies = ["vikrant rona", "james", "kgf", "kgf-2", ]
+        res.send(movies)
+    })
 module.exports = router;
+
+// 2 nd question
+router.get('/movies/:indexNumber', function(req, res) {
+    let movies = ["Rang de basanti", "the shining", "Lord of the rings", "Bats man begins"]
+    let index = req.params.indexNumber;
+    res.send(movies[index])
+
+});
+module.exports = router;
+
+
+
+// 3 rd question
+router.get('/movies/:indexNUmder', function(req, res) {
+    let movies = ["Rang de basanti", "the shining", "Lord of the rings", "Bats man begins"]
+    let index = req.params.indexNUmder;
+    if (index > movies.length) {
+        return res.send("use the valid index")
+    } else {
+        res.send(movies[index])
+    }
+})
+module.exports = router;
+
+
+// 4th question
+router.get('/get-films', function(req, res) {
+    let array = [{
+        id: 1,
+        name: "The Shining"
+    }, {
+        id: 2,
+        name: "ncendies"
+    }, {
+        id: 3,
+        name: "Rang de Basanti"
+    }, {
+        id: 4,
+        name: "Finding Nemo"
+    }]
+    res.send(array)
+})
+module.exports = router;
+
+
+// 5th question
+router.get('/get-films/:filmId', function(req, res) {
+    let array = [{
+        id: 1,
+        name: "The Shining"
+    }, {
+        id: 2,
+        name: "ncendies"
+    }, {
+        id: 3,
+        name: "Rang de Basanti"
+    }, {
+        id: 4,
+        name: "Finding Nemo"
+    }]
+
+    let index = req.params.filmId;
+    if (index > array.length) {
+        res.send("no movie exist with this Id")
+    } else {
+        res.send(array[index])
+
+    }
+})
+module.exports = router
