e6ac733331a2725782bc453e6e4ed31b3a6c1a01
MailarappaBudihal <104186059+Mailarappa-Budihal@users.noreply.github.com>
2022-08-25 12:00:05 +0530

    updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
+const UserController = require("../controllers/userController")
+const BookController = require("../controllers/bookController")
+const commonMW = require("../middlewares/commonMiddlewares")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
 
 
-router.post("/createBook", BookController.createBook  )
+
+
+// router.post("/createBook", BookController.createBook  )
 
 
 
