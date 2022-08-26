553994097af383609cc714c930bacded54afbe88
MailarappaBudihal <104186059+Mailarappa-Budihal@users.noreply.github.com>
2022-08-26 15:51:31 +0530

    middleware2

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(
         res.send({ msg: "done" })
     }
 );
-
+app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
